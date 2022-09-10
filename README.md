982e3017dcc7aeccbf2eab129f5a8bde3cad4a0e
alehunter <ahunt043@fiu.edu>
2022-09-10 18:36:08 -0400

    basic mouse drag functionality

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # odin-etchasketch
+
+Basic etch-a-sketch functionality.
+
+NxN grid is generated after user is prompted for N length.
+
+Drawing can be accomlished by click, and click and dragging the mouse.
--- javascript.js
+++ javascript.js
@@ -1,69 +1,101 @@
-document.body.style.display = 'flex';
-document.body.style.flexDirection = 'column';
+// document.body.style.width = '100%';
+// document.body.style.display = 'flex';
+// document.body.style.flexDirection = 'column';
+// document.body.style.justifyContent = 'center';
+// document.body.style.alignItems = 'center';
+// document.body.
 
-let grid = document.getElementById('grid');
+
+
+
+let grid = document.createElement('div');
 
 
 grid.style.cssText = ' \
-width: 100%;\
-height: 500px;\
+width: 95%; border: 2px solid red;\
+height: 50%;\
 display: flex;\
 flex-wrap: wrap;\
-border-collapse: collapse;';
+border-collapse: collapse;'
+;
+
+// let gridheight = grid.offsetWidth.toString();
+// gridheight = gridheight + 'px';
+// grid.style.height = gridheight;
+
+let length = prompt("How many squares across?");
+if(length > 100){
+    length = 100;
+}
 
 let squares = [];
 
-colors = ['red', 'yellow', 'blue'];
+let n = length;
+let calc = (100/n)*.894;
+let base = calc.toString();
+let str = base + '%';
 
-for(let i = 0; i < 25; i++){
+for(let i = 0; i < n*n; i++){
    let elem = document.createElement('div');
    elem.classList.add('square');
-   elem.style.width = '18%';
-   elem.style.height = '20%';
+   elem.style.width = str;
+   elem.style.height = str;
    elem.style.display = 'flex';
    elem.style.justifyContent = 'center';
    elem.style.alignItems = 'center';
-   elem.style.borderColor = 'red';
-   elem.style.borderWidth = '2px';
-   elem.style.borderStyle = 'solid';
+
+
+   if(i < n){
+    elem.style.backgroundColor = 'blue';
+   }
+   let isDown = false;
+ 
+
+   document.body.addEventListener('mousedown', function(){
+    isDown = true;
+    
+   })
+
+   document.body.addEventListener('mouseup', function(){
+    isDown = false;
+   })
+
+
+   elem.addEventListener('mousedown', function(){
+    isDown = true;
+    elem.style.backgroundColor = 'blue';
+   })
+
+   elem.addEventListener('mouseover', function(){
+    if(isDown == true){
+        elem.style.backgroundColor = 'blue';
+    }
    
-   elem.textContent = ''+ (i+1);
-   elem.setAttribute('clickamount', 0);
+   })
    
-   
-   elem.addEventListener('click', function(){
-    elem.style.backgroundColor = colors[elem.getAttribute('clickamount')];
-    elem.setAttribute('clickamount', (elem.getAttribute('clickamount')+1)%3);
-   });
+
    squares.push(elem);
    
    grid.appendChild(elem);
 }
+document.body.appendChild(grid);
+
 
 const squarearray = Array.from(squares);
 
-const reset = document.createElement('button');
-document.body.appendChild(reset);
-reset.textContent = "RESET";
-reset.style.fontSize = '30px';
-reset.style.width = 'fitcontent';
-reset.style.justifySelf = 'center';
-reset.style.alignSelf = 'center';
-reset.style.marginTop = '100px';
+// const reset = document.createElement('button');
+// reset.textContent = "RESET";
+// reset.style.fontSize = '30px';
+// reset.style.width = 'fitcontent';
 
-reset.addEventListener('click', function(){
-    for(let i = 0; i < squarearray.length; i++){
-        squarearray[i].setAttribute('clickamount', 0);
-        squarearray[i].style.backgroundColor = 'white';
-    }
-});
+// document.body.appendChild(reset);
 
-const directions = document.createElement('div');
-document.body.appendChild(directions);
 
-directions.textContent = 'Click element once for red, twice for blue and three times for yellow';
-directions.style.fontSize = '30px';
-directions.style.justifySelf = 'center';
-directions.style.alignSelf = 'center';
-directions.style.marginTop = '40px';
-directions.style.fontFamily = 'sans-serif';
+// reset.addEventListener('click', function(){
+//     for(let i = 0; i < squarearray.length; i++){
+//         squarearray[i].style.backgroundColor = 'white';
+        
+//     }
+// });
+
+
